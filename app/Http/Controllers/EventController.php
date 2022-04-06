e58620f09d717e1ad6d16d6267d4c4856a44afdb
yuusuke <uiternia0910@gmail.com>
2022-04-06 22:03:48 +0900

    eventShow

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -33,12 +33,6 @@ class EventController extends Controller
 
        $check =  EventService::checkDuplication($request['event_date'],$request['start_time'],$request['end_time']);
 
-        // $check = DB::table('events')
-        // ->whereDate('start_date',$request['event_date'])
-        // ->whereTime('end_date','>',$request['start_time'])
-        // ->whereTime('start_date','<',$request['end_time'])
-        // ->exists();
-
         if($check){
             session()->flash('status','この時間帯は他のイベントが存在します。');
             return view('chef.events.create');
@@ -47,15 +41,6 @@ class EventController extends Controller
         $startDate = EventService::joinDateAndTime($request['event_date'],$request['start_time']);
         $endDate = EventService::joinDateAndTime($request['event_date'],$request['end_time']);
 
-        // $start = $request['event_date'] . " " . $request['start_time'];
-        // $startDate = Carbon::createFromFormat(
-        //     'Y-m-d H:i', $start
-        // );
-
-        // $end = $request['event_date'] . " " . $request['end_time'];
-        // $endDate = Carbon::createFromFormat(
-        //     'Y-m-d H:i', $end
-        // );
 
         Event::create([
             'name' => $request['event_name'],
@@ -73,38 +58,26 @@ class EventController extends Controller
    
     public function show(Event $event)
     {
-        //
+        $event = Event::findOrFail($event->id);
+        $eventDate = $event->eventDate;
+        $startTime = $event->start_time;
+        $endTime = $event->end_time;
+        
+        return view('chef.events.show',compact('event','eventDate','startTime','endTime'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Event $event)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateEventRequest  $request
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(UpdateEventRequest $request, Event $event)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy(Event $event)
     {
         //
